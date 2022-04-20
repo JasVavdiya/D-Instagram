a7233b1b3617bf0bbc0f1e1a6beba676d363b158
jash <jash2212004@gmail.com>
2022-04-21 01:05:10 +0530

    add commit for adding contract to ropsten test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config()
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -8,6 +11,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
