733e0f6b86d3cc616642b0096dc7ba618fe8ccee
jash <jash2212004@gmail.com>
2022-04-20 23:32:54 +0530

    Updates

---
+++ precache-manifest.6b4da1935797cf34771f355f58b41628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed43faa5e8fc0b2fd81a",
+    "url": "/decentragram/static/css/main.ee35477f.chunk.css"
+  },
+  {
+    "revision": "ed43faa5e8fc0b2fd81a",
+    "url": "/decentragram/static/js/main.ed43faa5.chunk.js"
+  },
+  {
+    "revision": "ab12f83b4010ad34656b",
+    "url": "/decentragram/static/css/1.175cd769.chunk.css"
+  },
+  {
+    "revision": "ab12f83b4010ad34656b",
+    "url": "/decentragram/static/js/1.ab12f83b.chunk.js"
+  },
+  {
+    "revision": "a4a45a276db0bfa179c8",
+    "url": "/decentragram/static/js/runtime~main.a4a45a27.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentragram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "d283074c16409a542896e695354d9910",
+    "url": "/decentragram/index.html"
+  }
+];
